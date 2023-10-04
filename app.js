6fe6d1aff2ac0131262dd17f59adafd8c2856691
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-04 15:18:23 +0200

    Done

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+  return text.split('').reverse().join('');
+};
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+const seznam = document.querySelector('ul');
+const listItem = seznam.querySelectorAll('li');
+const arrayListItem = Array.from(listItem);
+console.log(arrayListItem);
+
+arrayListItem.forEach((prvek) => {
+  if (prvek.textContent === textPozpatku(prvek.textContent)) {
+    prvek.classList.add('palindrom');
+  }
+});
